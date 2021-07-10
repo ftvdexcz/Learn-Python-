8fe1c21055ea0181d2cf2c15a4da6fe40ca5893a
ftvdexcz <ftvdexc95@gmail.com>
2021-07-10 21:08:10 +0700

    update 5.py

--- Opencv/5.py
+++ Opencv/5.py
@@ -8,10 +8,18 @@ while True:
 	width = int(cap.get(3))
 	height = int(cap.get(4))
 
+	# Color detection (blue)
 	hsv_img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	lower_blue = np.array([110, 50, 50]) # chặn dưới 
+	upper_blue = np.array([130, 255, 255]) # chặn trên
 
-	cv2.imshow('frame', hsv_img)
+	# mask: phát hiện blue trong ảnh (cho biết nên giữ lại phần nào trong ảnh)
+	mask = cv2.inRange(hsv_img, lower_blue, upper_blue)
 
+	result = cv2.bitwise_and(frame, frame, mask=mask)
+
+	cv2.imshow('frame', result)
+	cv2.imshow('mask', mask)
 	if cv2.waitKey(1) == ord('q'):
 		break
 
